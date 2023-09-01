5974df0b95de5de6ebc8c63b2803ad31d0bb32bf
Glitch (create-a-jazzword) <none>
2023-09-01 18:00:33 +0000

    👙🤘 Checkpoint ./menumanager.py:17614628/526 ./forms.py:17614628/653

--- forms.py
+++ forms.py
@@ -9,3 +9,12 @@ class MenuForm(FlaskForm):
     name = StringField('Menu Name', validators=[DataRequired()])
     ingredients = FieldList(FormField(IngredientForm), min_entries=1)
     submit = SubmitField('Submit')
+
+    class Meta:
+        csrf = True
+        csrf_field_name = 'csrf_token'
+
+    def __init__(self, *args, **kwargs):
+        super(MenuForm, self).__init__(*args, **kwargs)
+        self.ingredients.meta = self.Meta()
+        self.ingredients.meta.csrf = False
