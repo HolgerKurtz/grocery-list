cdef57a5eaab303f071df9d317b6ce40d7588055
Glitch (create-a-jazzword) <none>
2023-09-02 08:06:27 +0000

    🛍🐽 Checkpoint ./menue.json:17614628/177 ./menumanager.py:17614628/1823 ./templates/index.html:17614628/156

--- menumanager.py
+++ menumanager.py
@@ -1,12 +1,14 @@
-import json
 import os
-
+import json
 
 class MenuManager:
     def __init__(self, file_path):
         self.file_path = file_path
         if not os.path.isfile(file_path):
             raise FileNotFoundError(f"The file {file_path} does not exist.")
+        self.load_data()
+
+    def load_data(self):
         with open(self.file_path, 'r', encoding='utf-8') as file:
             self.menu_data = json.load(file)
 
@@ -15,13 +17,13 @@ class MenuManager:
             print(menu_name)
 
     def get_ingredients_for_menu(self, menu_name):
+        self.load_data()  # Reload the data from the file
         return self.menu_data.get(menu_name, [])
 
     def add_menu(self, menu_name, ingredients):
         self.menu_data[menu_name] = ingredients
-        with open(self.file_path, 'w', encoding='utf-8') as file:
-            json.dump(self.menu_data, file, ensure_ascii=False)
-    
+        self.save()
+
     def save(self):
         with open(self.file_path, 'w') as file:
             json.dump(self.menu_data, file, indent=4)
