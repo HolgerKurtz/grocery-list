be22f2f4eb946b49b491e3da2f6fe3d811933f55
Glitch (grocery-list-julia) <none>
2023-09-05 11:28:42 +0000

    Rewound to commit 05811787086913332997a07fac66c164ba0b0a16 + 0 edits

--- static/main.js
+++ static/main.js
@@ -1,48 +1,50 @@
-function fetchIngredients() {
-  var selectedMenus = $(".menu-checkbox:checked")
-    .map(function () {
-      return $(this).val();
-    })
-    .get();
-  $.ajax({
-    url: "/get_ingredients",
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify({ menus: selectedMenus }),
-    success: function (response) {
-      if (response.error) {
-        alert(response.error);
-      } else {
-        displayIngredients(response.ingredients);
-      }
-    },
-  });
-}
+$(document).ready(function() {
 
-function displayIngredients(ingredients) {
-  // Convert array to Set to remove duplicates, then convert back to array
-  var uniqueIngredients = Array.from(new Set(ingredients));
-  // Sort the ingredients alphabetically
-  uniqueIngredients.sort();
-  $("#ingredients-list").html(
-    uniqueIngredients
-      .map(function (ingredient) {
-        return (
-          '<li class="list-group-item">' +
-          '<span class="ingredient-name">' +
-          ingredient +
-          "</span>" +
-          '<button class="btn btn-danger btn-sm float-right delete-button">✓</button>' +
-          "</li>"
+    function fetchIngredients() {
+        var selectedMenus = $(".menu-checkbox:checked")
+            .map(function () {
+                return $(this).val();
+            })
+            .get();
+        $.ajax({
+            url: "/get_ingredients",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ menus: selectedMenus }),
+            success: function (response) {
+                if (response.error) {
+                    alert(response.error);
+                } else {
+                    displayIngredients(response.ingredients);
+                }
+            },
+        });
+    }
+
+    function displayIngredients(ingredients) {
+        // Convert array to Set to remove duplicates, then convert back to array
+        var uniqueIngredients = Array.from(new Set(ingredients));
+        // Sort the ingredients alphabetically
+        uniqueIngredients.sort();
+        $("#ingredients-list").html(
+            uniqueIngredients
+                .map(function (ingredient) {
+                    return (
+                        '<li class="list-group-item">' +
+                        '<span class="ingredient-name">' + ingredient + '</span>' +
+                        '<button class="btn btn-danger btn-sm float-right delete-button">✓</button>' +
+                        '</li>'
+                    );
+                })
+                .join("")
         );
-      })
-      .join("")
-  );
-}
+    }
 
-function deleteIngredient() {
-  $(this).closest("li").remove();
-}
+    function deleteIngredient() {
+        $(this).closest('li').remove();
+    }
 
-$(".menu-checkbox").change(fetchIngredients);
-$("#ingredients-list").on("click", ".delete-button", deleteIngredient);
+    $(".menu-checkbox").change(fetchIngredients);
+    $("#ingredients-list").on('click', '.delete-button', deleteIngredient);
+
+});
